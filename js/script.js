8ca4fa8228dc978898e64151635142cfee54ab71
albert zhang <albertzz@bu.edu>
2025-02-04 13:07:28 -0500

    built whole website

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function addition() {
+    let first=document.getElementById("first").value;
+    let second=document.getElementById("second").value;
+    let result=Number(first)+Number(second);
+    document.getElementById("output").innerHTML=String(result);
+}
+
+function subtraction() {
+    let first=document.getElementById("first").value;
+    let second=document.getElementById("second").value;
+    let result=Number(first)-Number(second);
+    document.getElementById("output").innerHTML=String(result);
+
+    if (result < 0) {
+        document.getElementById("output").style.color = 'red';
+    } else {
+        document.getElementById("output").style.color = 'black';
+    }
+}
+
+function multiplication() {
+    let first=document.getElementById("first").value;
+    let second=document.getElementById("second").value;
+    let result=Number(first)*Number(second);
+    document.getElementById("output").innerHTML=String(result);
+
+    if (result < 0) {
+        document.getElementById("output").style.color = 'red';
+    } else {
+        document.getElementById("output").style.color = 'black';
+    }
+    }
+
+function division() {
+    let first=document.getElementById("first").value;
+    let second=document.getElementById("second").value;
+    let result=Number(first)/Number(second);
+    document.getElementById("output").innerHTML=String(result);
+
+    if (result < 0) {
+        document.getElementById("output").style.color = 'red';
+    } else {
+        document.getElementById("output").style.color = 'black';
+    }
+}
+
+function power() {
+    let first=Number(document.getElementById("first").value);
+    let second=Number(document.getElementById("second").value);
+    let result=1;
+    for(let i=0;i<second;i++){
+        result=first*result;
+    }
+
+    if (result < 0) {
+        document.getElementById("output").style.color = 'red';
+    } else {
+        document.getElementById("output").style.color = 'black';
+    }
+
+    document.getElementById("output").innerHTML=String(result);
+}
+
+function clearFields() {
+    document.getElementById("first").value = "";
+    document.getElementById("second").value = "";
+    document.getElementById("output").innerHTML = "";
+}
